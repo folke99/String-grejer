695acdfde5dd90714b4e3f933163df5d6c961f5c
folkefd <folkefd@gmail.com>
2016-09-30 14:45:21 +0200

    String

--- String övningar/Program.cs
+++ String övningar/Program.cs
@@ -10,6 +10,43 @@ namespace String_övningar
     {
         static void Main(string[] args)
         {
+            //Övning 1
+            /*
+            Console.WriteLine("\u0056\u00E4\u0157\u006D\u0064\u00F6\u0020\u0047\u0079\u006D\u006E\u0061\u0073\u0069\u0075\u006D");
+            Console.ReadKey();
+            */
+            //Övning 2
+            /*
+            Console.WriteLine("\"Hello world\"");
+            Console.ReadKey();
+            */
+            //Övning 3
+            /*
+            string y = "Hello world";
+            string x = y.ToUpper();
+            Console.WriteLine(x);
+            Console.ReadKey();
+            */
+            //Övning 4
+            /*
+            string y = "HELLO WORLD";
+            string x = y.ToLower();
+            Console.WriteLine(x);
+            Console.ReadKey();
+            */
+            //Övning 5
+
+            Console.WriteLine("Skriv in något:");
+            string inmatat = Console.ReadLine();
+
+            for (int i = inmatat.Length; i > inmatat.Length; i--)
+            {
+                Console.WriteLine(inmatat[i]);
+            }
+            Console.ReadKey();
+
+
+
         }
     }
 }
