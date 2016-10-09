f6b50924c378f560d81112c34e100a8bb914e8d6
folke <folke@DESKTOP-PUCU6K6>
2016-10-09 16:16:28 +0200

    String klart

--- String övningar/Program.cs
+++ String övningar/Program.cs
@@ -69,8 +69,35 @@ namespace String_övningar
             }
             Console.ReadKey();
             */
-            //Övning 7 INTE KLAR
+            //Övning 7
+            /*
+            string decimaler = "Decimaler:\t1\t2\t3";
+            string format = "\t\t{0:0.0}\t{0:0.00}\t{0:0.000}";
+            string e = string.Format(format, Math.E);
+            Console.WriteLine(decimaler + "\n" + e);
+            Console.ReadKey();
+            */
+            //Övning 8
+            /*
+            Console.WriteLine("Skriv in något:");
+            string inmatat = Console.ReadLine();
+            string bakNamn = "";
 
+            for (int i = inmatat.Length - 1; i >= 0; i--)
+            {
+                bakNamn = bakNamn + inmatat[i];
+            }
+
+            if (inmatat == bakNamn)
+            {
+                Console.WriteLine(inmatat + " är ett palidrom");
+            }
+            else
+            {
+                Console.WriteLine(inmatat + " är inte ett palidrom");
+            }
+            Console.ReadKey();
+            /*
             // Övning 2.1
             /*
             DateTime dt = DateTime.Now;
@@ -89,8 +116,36 @@ namespace String_övningar
             Console.ReadKey();
             */
             //Övning 2.4
-
-
+            /*
+            double tal = 123.00;
+            string format = "{0:0}";
+            string format2 = "{0:00}";
+            var tal2 = tal - Math.Truncate(tal);
+            string text = string.Format(format, tal);
+            string text2 = string.Format(format2, tal2);
+            Console.WriteLine("Du har " + text + " kroner och " + text2 + " öre");
+            Console.ReadKey();
+            */
+            //Övning 2.5
+            /*
+            Console.Write("Mata in ditt personnummer:");
+            double persNum = double.Parse(Console.ReadLine());
+            string format = "{0:####,##,## - ####}";
+            string ut = string.Format(format, persNum);
+            Console.WriteLine("Är ditt personnummer: " + ut);
+            Console.ReadKey();
+            */
+            //Övning 2.6
+            /*
+            const string space = "{0,-15}{1,-15}{2,-15}{3,-15}{4,-15}";
+            Console.WriteLine("--------------------------------Veckoschema----------------------------------");
+            Console.WriteLine(string.Format(space, "Måndag", "| Tisdag", "| Onsdag", "| Torsdag", "| Fredag"));
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.WriteLine(string.Format(space, "Matematik", "| Engelska", "| Teknik", "| Idrott", "| Programmering"));
+            Console.WriteLine(string.Format(space, "Svenska", "| Historia", "| Matematik", "| Matematik", "| Webbprogrammering"));
+            Console.WriteLine("-----------------------------------------------------------------------------");
+            Console.ReadKey();
+            */
         }
     }
 }
